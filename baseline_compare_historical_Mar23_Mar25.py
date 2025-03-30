ac348347beb018675786a3781e11dc5bb997a74d
dviladrich95 <daniel.viladrich.h@gmail.com>
2025-03-30 01:33:36 +0100

    renamed and reworked historical analysis scripts

--- baseline_compare_historical_Mar23_Mar25.py
+++ baseline_compare_historical_Mar23_Mar25.py
@@ -7,7 +7,7 @@ from strom.plot_utils import plot_combined_cases
 import pandas as pd
 import matplotlib.pyplot as plt
 
-temp_price_df = pd.read_csv('data/Temp_Price_Barcelona_Nov.csv', index_col="Timestamp", parse_dates=["Timestamp"])
+temp_price_df = pd.read_csv('data/Temp_Price_Barcelona_Mar23_Mar25.csv', index_col="Timestamp", parse_dates=["Timestamp"])
 
 house = House(
     C_air = 0.56,
@@ -15,12 +15,15 @@ house = House(
     R_interior = 1.0,
     R_exterior = 6.06,
     Q_heater = 2.0,
+    Q_cooling = 2.0,
     T_min = 18.0,
     T_max = 24.0,
     T_interior_init = 18.5,
     T_wall_init = 18.5,
     P_base = 0.01,
-    freq = '15min')
+    freq = '15min',
+    cooling_unit = True
+    )
 
 optimal_state_df, baseline_state_df = compare_output_costs(temp_price_df,house)
 
